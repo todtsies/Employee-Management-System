d61d12728db12954068c66e755794a34656e1276
todtsies <emilybtodt@gmail.com>
2021-02-23 12:39:49 -0500

    Update employees_db.sql
    Add query to view all roles

--- employees_db.sql
+++ employees_db.sql
@@ -37,3 +37,6 @@ VALUES ("Marion", "Knight", "1"), ("Bill", "Gates", "2", "1"), ("Rob", "Robson",
 
 -- Query for view all --
 SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
+
+-- Query for view all roles --
+SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
