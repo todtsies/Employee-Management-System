a6301c9d92d9f3ac0cd7be78807d1993c676073c
todtsies <emilybtodt@gmail.com>
2021-02-23 13:07:23 -0500

    Create connection to database

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const { printTable } = require('console-table-printer');
 const figlet = require('figlet');
+let roles;
+let departments;
+let managers;
+let employees;
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Colton123!",
+    database: "employees_db"
+  });
